bab981e984035843af6521b376c137fbdf01ecac
Tri Dung Duong <trduong@atlas2.eng.uts.edu.au>
2020-07-14 19:24:39 +1000

    update code

---
+++ Source Code/my_work/config/experiment_config.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+## Output path
+RESULT_PATH = "/home/trduong/Data/interpretable_machine_learning/Source Code/my_work/result"
+FIGURE_PATH = "/home/trduong/Data/interpretable_machine_learning/Source Code/my_work/figure"
+
+## Data path
+IHDP_TEST = "/home/trduong/Data/data/ihdp_npci_1-100.test.npz"
+IHDP_TRAIN = "/home/trduong/Data/data/ihdp_npci_1-100.train.npz"
+
+
