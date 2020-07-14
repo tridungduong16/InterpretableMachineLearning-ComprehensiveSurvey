e9504c0cdb6bd1b8f4265bcae9141278f6fe0256
tridungduong16 <tridungduong16@gmail.com>
2020-07-14 20:45:18 +1000

    update optimization

--- Source Code/my_work/config/experiment_config.py
+++ Source Code/my_work/config/experiment_config.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 
 ## Output path
-RESULT_PATH = "/home/trduong/Data/interpretable_machine_learning/Source Code/my_work/result"
-FIGURE_PATH = "/home/trduong/Data/interpretable_machine_learning/Source Code/my_work/figure"
+RESULT_PATH = "/home/dtd/Documents/PhD_Life/Source Code/my_work/result"
+FIGURE_PATH = "/home/dtd/Documents/PhD_Life/Source Code/my_work/figure"
 
 ## Data path
-IHDP_TEST = "/home/trduong/Data/data/ihdp_npci_1-100.test.npz"
-IHDP_TRAIN = "/home/trduong/Data/data/ihdp_npci_1-100.train.npz"
+IHDP_TEST = "/home/dtd/Documents/data/idhp/ihdp_npci_1-100.test.npz"
+IHDP_TRAIN = "/home/dtd/Documents/data/idhp/ihdp_npci_1-100.train.npz"
 
 
