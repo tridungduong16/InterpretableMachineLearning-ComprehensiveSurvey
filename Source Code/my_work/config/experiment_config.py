d8e2ed271fec944495856c4fa0e9775ef22e0d3c
Tri Dung Duong <trduong@atlas2.eng.uts.edu.au>
2020-07-15 10:57:42 +1000

    update

--- Source Code/my_work/config/experiment_config.py
+++ Source Code/my_work/config/experiment_config.py
@@ -2,11 +2,12 @@ import pandas as pd
 import numpy as np
 
 ## Output path
-RESULT_PATH = "/home/dtd/Documents/PhD_Life/Source Code/my_work/result"
-FIGURE_PATH = "/home/dtd/Documents/PhD_Life/Source Code/my_work/figure"
+MAIN_PATH = "/home/trduong/Data/interpretable_machine_learning/Source Code/my_work"
+RESULT_PATH = MAIN_PATH + "/result"
+FIGURE_PATH = MAIN_PATH + "/figure"
 
 ## Data path
-IHDP_TEST = "/home/dtd/Documents/data/idhp/ihdp_npci_1-100.test.npz"
-IHDP_TRAIN = "/home/dtd/Documents/data/idhp/ihdp_npci_1-100.train.npz"
+IHDP_TEST = "/home/trduong/Data/data/ihdp_npci_1-100.test.npz"
+IHDP_TRAIN = "/home/trduong/Data/data/ihdp_npci_1-100.train.npz"
 
 
